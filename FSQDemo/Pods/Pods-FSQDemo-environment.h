1ad26cc0291c30b1979e1d618cba441d04f265c3
kimuraw <kimuraw@i.nifty.jp>
2013-09-22 15:14:38 +0900

    Done 'pod instsall'

---
+++ FSQDemo/Pods/Pods-FSQDemo-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Foursquare-iOS-API
+#define COCOAPODS_POD_AVAILABLE_Foursquare_iOS_API
+#define COCOAPODS_VERSION_MAJOR_Foursquare_iOS_API 1
+#define COCOAPODS_VERSION_MINOR_Foursquare_iOS_API 1
+#define COCOAPODS_VERSION_PATCH_Foursquare_iOS_API 4
+
+// Foursquare-iOS-API/Core+SBJson
+#define COCOAPODS_POD_AVAILABLE_Foursquare_iOS_API_Core_SBJson
+#define COCOAPODS_VERSION_MAJOR_Foursquare_iOS_API_Core_SBJson 1
+#define COCOAPODS_VERSION_MINOR_Foursquare_iOS_API_Core_SBJson 1
+#define COCOAPODS_VERSION_PATCH_Foursquare_iOS_API_Core_SBJson 4
+
+// SBJson
+#define COCOAPODS_POD_AVAILABLE_SBJson
+#define COCOAPODS_VERSION_MAJOR_SBJson 3
+#define COCOAPODS_VERSION_MINOR_SBJson 2
+#define COCOAPODS_VERSION_PATCH_SBJson 0
+
